2eb0af48d58695844632d53ce5d482e5c1a48ac5
yamadapc <tacla.yamada@gmail.com>
2014-05-26 12:03:10 -0300

    Slightly improve the example.
    I'm trying to mimick asciimoo's basic.py example.

--- examples/Senoid.hs
+++ examples/Senoid.hs
@@ -1,13 +1,24 @@
 import Data.Angle
+import Data.List (union)
+import Control.Arrow ((&&&))
 import qualified System.Drawille as D
 
 main :: IO ()
 main = do
-    let c = D.fromList $ take 1800 senoid
-    putStr $ D.frame c
+    let s = D.fromList $ take 1800 senoid
+    putStr $ D.frame s
+
+    let cs = D.fromList $ union (take 1800 senoid) (take 1800 cosenoid)
+    putStr $ D.frame cs
 
 senoid :: Integral a => [(a, a)]
-senoid = zip (map (`div` 10) [0..]) (map (floor . (* 10) . rsine) [0..])
+senoid = map ((`div` 10) &&& (floor . (* 10) . rsine . fromIntegral)) [0..]
+
+cosenoid :: Integral a => [(a, a)]
+cosenoid = map ((`div` 10) &&& (floor . (* 10) . rcosine . fromIntegral)) [0..]
 
 rsine :: Double -> Double
 rsine = sine . radians . Degrees
+
+rcosine :: Double -> Double
+rcosine = cosine . radians . Degrees
